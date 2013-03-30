5b80eb3096ebcf9042cc7a878a25497cf0f41895
kautiontape <kautiontape@gmail.com>
2013-03-30 14:34:35 -0400

    Refactored code a little, removed tab bar, removed reference to list

--- src/edu/umbc/teamawesome/assignment2/MainActivity.java
+++ src/edu/umbc/teamawesome/assignment2/MainActivity.java
@@ -1,6 +1,7 @@
 package edu.umbc.teamawesome.assignment2;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.Locale;
@@ -16,7 +17,6 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.GoogleMapOptions;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 import android.location.Criteria;
@@ -40,13 +40,9 @@ import android.app.FragmentTransaction;
 import android.app.ActionBar.Tab;
 
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ListView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class MainActivity extends Activity implements SensorEventListener  {
 	
@@ -69,7 +65,8 @@ public class MainActivity extends Activity implements SensorEventListener  {
 	GoogleMap map;
 	MapFragment mapFragment;
 	LocationManager locationManager;
-	List<PinInformation> pinList;
+	List<PinInformation> pinList = new ArrayList<PinInformation>();
+	//ArrayAdapter<PinInformation> lv_adapter;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) 
@@ -77,6 +74,233 @@ public class MainActivity extends Activity implements SensorEventListener  {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
+		//createTabBar();
+        
+		registerLocationListener();
+		registerSensorListeners();
+		registerInterfaceListeners();
+		
+		createMap();
+		 
+		instantiateFromDatabase();
+		updatePins();
+	}
+	
+	private void newLocation(Location newLocation) 
+	{
+		if(lastLocation != null) 
+		{
+			float distance = lastLocation.distanceTo(newLocation);
+			if(distance > 100) {
+				this.newLocation = newLocation;
+				saveInformation();
+				this.lastLocation = newLocation;
+			}
+		}
+		else 
+		{
+			// we set need to set both locations to avoid null pointer exceptions in code above
+			// will represent no movement, so not a semantic error
+			this.lastLocation = newLocation;
+			this.newLocation = newLocation;
+			saveInformation();
+		}
+		
+		updatePins();
+	}
+	
+	private void saveInformation() {		
+		PinInformation pin = new PinInformation();
+		pin.setId(++pins); // Pin count is updated here
+		pin.setTime(System.currentTimeMillis());
+		pin.setLongitude(newLocation.getLongitude());
+		pin.setLatitude(newLocation.getLatitude());
+		pin.setAccel_x(accel[0]);
+		pin.setAccel_y(accel[1]);
+		pin.setAccel_z(accel[2]);
+		pin.setOrient_x(orient[0]);
+		pin.setOrient_y(orient[1]);
+		pin.setOrient_z(orient[2]);
+		pin.setLx(lx);
+		pin.setProx(prox);
+		pin.setActivity(activity);
+		
+		Log.d(getPackageName(), "Added new pin");
+		
+		List<Address> addressList = null;
+		Geocoder geocoder = new Geocoder(getApplicationContext(), Locale.getDefault());   
+		try {
+			addressList = geocoder.getFromLocation(newLocation.getLatitude(), newLocation.getLongitude(), 1);
+		} 
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		if(addressList != null && !addressList.isEmpty())
+		{
+			//pin.addAddress(addressList.get(0);
+		}
+		
+		db.addEntry(pin);
+		
+		addMarker(pin);
+	}
+	
+	private void addMarker(PinInformation pin)
+	{
+		MarkerOptions marker = new MarkerOptions();
+		marker.draggable(false);
+		marker.position(new LatLng(newLocation.getLatitude(), newLocation.getLongitude()));
+		marker.title(activity);
+		
+		String address = "";
+		
+		String disp = String.format(Locale.US,
+				"Time: %s" + 
+				"%n<%f, %f>" +
+				"%nAccel: (%.2f, %.2f, %.2f)" +
+				"%nOrient: (%.2f, %.2f, %.2f)" +
+				"%nLx: %.2f" +
+				"%nProximity: %.2f" +
+				(address.length() > 0 ? "%nAddress: " + address : "") +
+				(pin.getActivity().length() > 0 ? "%nActivity: " + pin.getActivity() : ""),
+				new java.text.SimpleDateFormat("dd/MM/yyyy HH:mm:ss", Locale.US).format(new Date(pin.getTime())),
+				pin.getLongitude(), pin.getLatitude(), pin.getAccel_x(), 
+				pin.getAccel_y(), pin.getAccel_z(), pin.getOrient_x(), 
+				pin.getOrient_y(), pin.getOrient_z(), pin.getLx(), pin.getProx()
+		);
+		marker.snippet(disp);
+		
+		map.addMarker(marker);
+	}
+	
+	private void updatePins() 
+	{
+		pinList = db.getAllPins();
+		//lv_adapter.notifyDataSetChanged();
+		
+		if(map != null)
+		{
+			map.clear();
+			
+			for(PinInformation pin: pinList)
+			{
+				addMarker(pin);
+			}
+		}
+	}
+	
+	// http://www.androidsnippets.com/prompt-user-input-with-an-alertdialog
+	private void promptActivity() {
+		AlertDialog.Builder alert = new AlertDialog.Builder(this);
+		alert.setTitle("628 Assignment 2");
+		alert.setMessage("What are you currently doing?");
+		
+		final EditText input = new EditText(this);
+		alert.setView(input);
+		
+		alert.setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+			public void onClick(DialogInterface dialog, int whichButton) {
+				activity = input.getText().toString();
+				updateActivityView();
+			 }
+		});
+
+		alert.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+		  public void onClick(DialogInterface dialog, int whichButton) {}
+		});
+		
+		alert.show();
+	}
+	
+	private void registerSensorListeners() {
+		SensorManager manager = (SensorManager)getSystemService(Context.SENSOR_SERVICE);
+		Sensor accelerometer = manager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+		Sensor magnetometer = manager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+		Sensor light = manager.getDefaultSensor(Sensor.TYPE_LIGHT);
+		Sensor proximity = manager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
+
+		manager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+		manager.registerListener(this, magnetometer, SensorManager.SENSOR_DELAY_NORMAL);
+		manager.registerListener(this, light, SensorManager.SENSOR_DELAY_NORMAL);
+		manager.registerListener(this, proximity, SensorManager.SENSOR_DELAY_NORMAL);
+	}
+	
+	private void registerInterfaceListeners() {	
+//		ListView lv = (ListView)findViewById(R.id.pinList);
+//		lv_adapter = new ArrayAdapter<PinInformation>(this, android.R.layout.simple_list_item_1, pinList);
+//		lv.setAdapter(lv_adapter);
+		
+		Button buttonClear = (Button)findViewById(R.id.buttonClear);
+		buttonClear.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				db.clearAll();
+				updatePins();
+			}
+		});
+		
+		TextView activityText = (TextView)findViewById(R.id.textActivity);
+		activityText.setOnClickListener(new View.OnClickListener() {			
+			@Override
+			public void onClick(View v) {
+				promptActivity();
+			}
+		});
+	}
+	
+	private void registerLocationListener() {
+		locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+		
+		LocationListener locationListener = new LocationListener() {
+			
+			@Override
+			public void onLocationChanged(Location location) {
+				newLocation(location);
+			}
+			
+			@Override
+			public void onStatusChanged(String provider, int status, Bundle extras) {}
+			
+			@Override
+			public void onProviderEnabled(String provider) {}
+			
+			@Override
+			public void onProviderDisabled(String provider) {}
+		};
+		
+		locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+	}
+	
+	private void createMap() {
+		GoogleMapOptions options = new GoogleMapOptions();
+		
+		options.mapType(GoogleMap.MAP_TYPE_NORMAL).compassEnabled(true).rotateGesturesEnabled(true).tiltGesturesEnabled(true);
+		mapFragment = MapFragment.newInstance(options);
+		
+		FragmentTransaction fragmentTransaction =
+		         getFragmentManager().beginTransaction();
+		fragmentTransaction.add(R.id.map_container, mapFragment);
+		fragmentTransaction.commit();
+	}
+	
+	private void instantiateFromDatabase() {		
+		db = new DatabaseHandler(this);
+		pins = db.getPinCount();
+		if(pins > 0) {
+			PinInformation lastPin = db.getPin(pins);
+			Location loc = new Location(LOCATION_SERVICE);
+			loc.setLatitude(lastPin.getLatitude());
+			loc.setLongitude(lastPin.getLongitude());
+			newLocation = lastLocation = loc;
+			activity = lastPin.getActivity();
+			updateActivityView();
+		}
+	}
+	
+	@SuppressWarnings("unused")
+	private void createTabBar() {
+
 		final ActionBar bar = getActionBar();
         bar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
         bar.setDisplayOptions(0, ActionBar.DISPLAY_SHOW_TITLE);
@@ -129,230 +353,6 @@ public class MainActivity extends Activity implements SensorEventListener  {
 					public void onTabReselected(Tab arg0, FragmentTransaction arg1) {
 					}
 				}));
-        
-		registerLocationListener();
-		registerSensorListeners();
-		registerInterfaceListeners();
-		
-		GoogleMapOptions options = new GoogleMapOptions();
-		
-		options.mapType(GoogleMap.MAP_TYPE_NORMAL).compassEnabled(true).rotateGesturesEnabled(true).tiltGesturesEnabled(true);
-		mapFragment = MapFragment.newInstance(options);
-		
-		FragmentTransaction fragmentTransaction =
-		         getFragmentManager().beginTransaction();
-		fragmentTransaction.add(R.id.map_container, mapFragment);
-		fragmentTransaction.commit();
-		 
-		instantiateFromDatabase();
-		updatePins();
-	}
-	
-	private void newLocation(Location newLocation) 
-	{
-		//this only records the new location if it's a 100m+ step, since the last location update, but shouldn't lastLocation
-		//only update once newLocation has more than 100m difference instead? so in the else statement, lastLocation doesn't update?
-		if(lastLocation != null) 
-		{
-			float distance = lastLocation.distanceTo(newLocation);
-			if(distance > 100) {
-				this.newLocation = newLocation;
-				saveInformation();
-				this.lastLocation = newLocation;
-			}
-		}
-		else 
-		{
-			this.lastLocation = newLocation;
-			this.newLocation = newLocation;
-			saveInformation();
-		}
-		
-		updatePins();
-	}
-	
-	private void saveInformation() {		
-		PinInformation pin = new PinInformation();
-		pin.setId(++pins); // Pin count is updated here
-		pin.setTime(System.currentTimeMillis());
-		pin.setLongitude(newLocation.getLongitude());
-		pin.setLatitude(newLocation.getLatitude());
-		pin.setAccel_x(accel[0]);
-		pin.setAccel_y(accel[1]);
-		pin.setAccel_z(accel[2]);
-		pin.setOrient_x(orient[0]);
-		pin.setOrient_y(orient[1]);
-		pin.setOrient_z(orient[2]);
-		pin.setLx(lx);
-		pin.setProx(prox);
-		pin.setActivity(activity);
-		
-		Log.d(getPackageName(), "Added new pin");
-		
-		List<Address> addressList = null;
-		Geocoder geocoder = new Geocoder(getApplicationContext(), Locale.getDefault());   
-		try {
-			addressList = geocoder.getFromLocation(newLocation.getLatitude(), newLocation.getLongitude(), 1);
-		} 
-		catch (IOException e) {
-			e.printStackTrace();
-		}
-		
-		if(addressList != null && !addressList.isEmpty())
-		{
-			//pin.addAddress(addressList.get(0);
-		}
-		
-		db.addEntry(pin);
-		
-		addMarker(pin);
-	}
-	
-	private void addMarker(PinInformation pin)
-	{
-		MarkerOptions marker = new MarkerOptions();
-		marker.draggable(false);
-		marker.position(new LatLng(newLocation.getLatitude(), newLocation.getLongitude()));
-		marker.title(activity);
-		marker.snippet(pin.toString());
-		
-		map.addMarker(marker);
-	}
-	
-	private void updatePins() 
-	{
-		pinList = db.getAllPins();
-		ListView lv = (ListView)findViewById(R.id.pinList);
-		ArrayAdapter<PinInformation> adapter = new ArrayAdapter<PinInformation>(this, android.R.layout.simple_list_item_1, pinList);
-		lv.setAdapter(adapter);
-		
-		if(map != null)
-		{
-			map.clear();
-			
-			for(PinInformation pin: pinList)
-			{
-				addMarker(pin);
-			}
-		}
-	}
-	
-	// http://www.androidsnippets.com/prompt-user-input-with-an-alertdialog
-	private void promptActivity() {
-		AlertDialog.Builder alert = new AlertDialog.Builder(this);
-		alert.setTitle("628 Assignment 2");
-		alert.setMessage("What are you currently doing?");
-		
-		final EditText input = new EditText(this);
-		alert.setView(input);
-		
-		alert.setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
-			public void onClick(DialogInterface dialog, int whichButton) {
-				activity = input.getText().toString();
-				updateActivityView();
-			 }
-		});
-
-		alert.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
-		  public void onClick(DialogInterface dialog, int whichButton) {}
-		});
-		
-		alert.show();
-	}
-	
-	private void registerSensorListeners() {
-		SensorManager manager = (SensorManager)getSystemService(Context.SENSOR_SERVICE);
-		Sensor accelerometer = manager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-		Sensor magnetometer = manager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
-		Sensor light = manager.getDefaultSensor(Sensor.TYPE_LIGHT);
-		Sensor proximity = manager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
-
-		manager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
-		manager.registerListener(this, magnetometer, SensorManager.SENSOR_DELAY_NORMAL);
-		manager.registerListener(this, light, SensorManager.SENSOR_DELAY_NORMAL);
-		manager.registerListener(this, proximity, SensorManager.SENSOR_DELAY_NORMAL);
-	}
-	
-	private void registerInterfaceListeners() {
-		ListView lv = (ListView)findViewById(R.id.pinList);
-		lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-			@Override
-			public void onItemClick(AdapterView<?> arg0, View arg1, int position, long arg3) {
-				PinInformation pin = (PinInformation)arg0.getItemAtPosition(position);
-				
-				String address = "";
-				
-				String disp = String.format(Locale.US,
-						"Time: %s" + 
-						"%n<%f, %f>" +
-						"%nAccel: (%.2f, %.2f, %.2f)" +
-						"%nOrient: (%.2f, %.2f, %.2f)" +
-						"%nLx: %.2f" +
-						"%nProximity: %.2f" +
-						(address.length() > 0 ? "%nAddress: " + address : "") +
-						(pin.getActivity().length() > 0 ? "%nActivity: " + pin.getActivity() : ""),
-						new java.text.SimpleDateFormat("dd/MM/yyyy HH:mm:ss", Locale.US).format(new Date(pin.getTime())),
-						pin.getLongitude(), pin.getLatitude(), pin.getAccel_x(), 
-						pin.getAccel_y(), pin.getAccel_z(), pin.getOrient_x(), 
-						pin.getOrient_y(), pin.getOrient_z(), pin.getLx(), pin.getProx()
-				);
-				Toast.makeText(getApplicationContext(), disp, Toast.LENGTH_SHORT).show();
-			}
-		});
-		
-		Button buttonClear = (Button)findViewById(R.id.buttonClear);
-		buttonClear.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				db.clearAll();
-				updatePins();
-			}
-		});
-		
-		TextView activityText = (TextView)findViewById(R.id.textActivity);
-		activityText.setOnClickListener(new View.OnClickListener() {			
-			@Override
-			public void onClick(View v) {
-				promptActivity();
-			}
-		});
-	}
-	
-	private void registerLocationListener() {
-		locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
-		
-		LocationListener locationListener = new LocationListener() {
-			
-			@Override
-			public void onLocationChanged(Location location) {
-				newLocation(location);
-			}
-			
-			@Override
-			public void onStatusChanged(String provider, int status, Bundle extras) {}
-			
-			@Override
-			public void onProviderEnabled(String provider) {}
-			
-			@Override
-			public void onProviderDisabled(String provider) {}
-		};
-		
-		locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
-	}
-	
-	private void instantiateFromDatabase() {		
-		db = new DatabaseHandler(this);
-		pins = db.getPinCount();
-		if(pins > 0) {
-			PinInformation lastPin = db.getPin(pins);
-			Location loc = new Location(LOCATION_SERVICE);
-			loc.setLatitude(lastPin.getLatitude());
-			loc.setLongitude(lastPin.getLongitude());
-			newLocation = lastLocation = loc;
-			activity = lastPin.getActivity();
-			updateActivityView();
-		}
 	}
 	
 	private void updateActivityView() 
